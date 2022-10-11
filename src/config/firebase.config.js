fd969b53ddcc2cc95a4f8d0d559aa35b18c6248d
Raiyan109 <kraiyan109@gmail.com>
2022-10-11 19:39:41 +0600

    firebase initialize

---
+++ src/config/firebase.config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy_Z2sad2KM-zdQlwufumjgn4WOXyWFo0",
+  authDomain: "daily-mail-f0806.firebaseapp.com",
+  projectId: "daily-mail-f0806",
+  storageBucket: "daily-mail-f0806.appspot.com",
+  messagingSenderId: "159724129960",
+  appId: "1:159724129960:web:57d15751653f59baecbc9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
