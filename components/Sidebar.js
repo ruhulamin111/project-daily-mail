998434349917b5bfef580ec0cc8f0252d1c2316a
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-12 21:30:48 +0600

    remve data

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,7 +17,6 @@ export default function Sidebar() {
             <Button
                 startIcon={<Add />}
                 className={styles.sidebar__composeBtn}
-                fonSize='large'
             >
                 Compose
             </Button>
