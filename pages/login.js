de57a15307fd24cd32c3e8ecc63b6718e052667e
Raiyan109 <kraiyan109@gmail.com>
2022-10-12 15:09:25 +0600

    image

--- pages/login.js
+++ pages/login.js
@@ -2,15 +2,16 @@ import React from 'react';
 import styles from '../styles/Login.module.css'
 import logo from '../assets/Asset.png'
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 
 const Login = () => {
     const { data: session } = useSession()
     return (
         <div className={styles.login}>
-            <div className={styles.login_container}>
-                <img src={logo} alt="" />
-                login
+            <div className={styles.loginContainer}>
+                <Image src='/../public/assets/Asset.png' alt='image' width='125' height='50' />
+
             </div>
         </div>
     )
