f6fa424021ceb64bb1f9a88702e095ebe1f08549
Raiyan109 <kraiyan109@gmail.com>
2022-10-12 15:03:49 +0600

    auth running

--- pages/login.js
+++ pages/login.js
@@ -3,27 +3,17 @@ import styles from '../styles/Login.module.css'
 import logo from '../assets/Asset.png'
 import { useSession, signIn, signOut } from 'next-auth/react';
 
-// const Login = () => {
-//     const { data: session } = useSession()
-//     return (
-//         <div className={styles.login}>
-//             <div className={styles.login_container}>
-//                 <img src={logo} alt="" />
-//             </div>
-//         </div>
-//     );
-// };
 
-// export default Login;
-
-export default function Login() {
+const Login = () => {
     const { data: session } = useSession()
     return (
         <div className={styles.login}>
             <div className={styles.login_container}>
                 <img src={logo} alt="" />
+                login
             </div>
         </div>
     )
 }
 
+export default Login;
