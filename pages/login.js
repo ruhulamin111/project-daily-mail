dc8c4fc9199e5b96d38f3595ad2607f494801fe2
Raiyan109 <kraiyan109@gmail.com>
2022-10-12 15:18:54 +0600

    assets moved on public, google auth worked properly

--- pages/login.js
+++ pages/login.js
@@ -7,14 +7,24 @@ import Image from 'next/image';
 
 const Login = () => {
     const { data: session } = useSession()
-    return (
-        <div className={styles.login}>
-            <div className={styles.loginContainer}>
-                <Image src='/../public/assets/Asset.png' alt='image' width='125' height='50' />
-
+    if (session) {
+        return (
+            <div className={styles.login}>
+                <div className={styles.loginContainer}>
+                    <Image src='/../public/assets/Asset.png' alt='image' width='125' height='50' />
+                    <p>welcome, {session.user.email}</p>
+                    <button onClick={() => signOut()}>Sign out</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    else {
+        return (
+            <div><p>You are not signed in</p>
+                <button onClick={() => signIn()}>Sign in</button>
+            </div>
+        )
+    }
 }
 
 export default Login;
